1f8aac32ca24426db5289d6e2d4c6fb4dcca4581
Inci <inciqenberli22@gmail.com>
2026-05-08 17:54:52 +0400

    Add JWT authentication filter and custom UserDetailsService implementation

---
+++ src/main/java/com/malik/earthquakeapi/EarthquakeApiApplication.java
@@ -0,0 +1,12 @@
+package com.malik.earthquakeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthquakeApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/malik/earthquakeapi/EarthquakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malik.earthquakeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
